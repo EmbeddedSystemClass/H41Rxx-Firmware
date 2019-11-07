695942c89b19228a0da478ddabb9ee1e4dc46628
Asaad K <masaadkaadan@gmail.com>
2019-11-07 13:54:02 +0200

    Added memory map for F745

--- H41Rx/H41Rx_MemoryMap.h
+++ H41Rx/H41Rx_MemoryMap.h
@@ -14,20 +14,36 @@
  extern "C" {
 #endif
 
-/* Memory map: - STM32F091CB
-				- Application: 0x08000000 - 0x0801D7FF >> 118 KB
-		 - Read-only (RO): 0x0801D800 - 0x0801DFFF >> 2 KB, used to store topology information and Command Snippets
-		- Emulated EEPROM: 0x0801E000 - 0x0801FFFF >> 8 KB, fits 1024 16-bit variables in 2 main-duplicate pages (A and B)
-*/
-#define APP_START_ADDRESS  		((uint32_t)0x08000000) 
-#define RO_START_ADDRESS  		((uint32_t)0x0801D800) 
-#define RO_MID_ADDRESS  			((uint32_t)0x0801DC00) 	// Snippets are stored here
-#define EEPROM_START_ADDRESS  ((uint32_t)0x0801E000) 
-#define FLASH_SIZE						((uint32_t)0x20000)			// All sizes in bytes
-#define SRAM_SIZE							((uint32_t)0x8000)
-#define PAGE_SIZE             ((uint32_t)0x0800)  		/* Page size = 2KByte for STM32F07x and STM32F09x devices */
-#define NumOfPages						(FLASH_SIZE/PAGE_SIZE)
+/* Memory map: 	- STM32F745VGT6 LQFP-100 MCU
+  				- Embedded FLASH memory accessible over AXI: 0x08000000 - 0x080FFFFF (1 MB)
+  				- SRAM1 accessible over AXI/AHB: 0x20010000 - 0x2003C000 (240KB)
+  				- SRAM2 accessible over AXI/AHB: 0x2004C000 - 0x20050000 (16KB)
+  				- STM32F756xx and STM32F74xxx Flash memory organization
+  								AXIM Interface				ITCM Interface
+					Sector 0 0x08000000 - 0x08007FFF 	0x00200000 - 0x00207FFF 32 Kbytes
+					Sector 1 0x08008000 - 0x0800FFFF 	0x00208000 - 0x0020FFFF 32 Kbytes
+					Sector 2 0x08010000 - 0x08017FFF 	0x00210000 - 0x00217FFF 32 Kbytes
+					Sector 3 0x08018000 - 0x0801FFFF 	0x00218000 - 0x0021FFFF 32 Kbytes
+					Sector 4 0x08020000 - 0x0803FFFF 	0x00220000 - 0x0023FFFF 128 Kbytes
+					Sector 5 0x08040000 - 0x0807FFFF 	0x00240000 - 0x0027FFFF 256 Kbytes
+					Sector 6 0x08080000 - 0x080BFFFF 	0x00280000 - 0x002BFFFF 256 Kbytes
+					Sector 7 0x080C0000 - 0x080FFFFF 	0x002C0000 - 0x002FFFFF 256 Kbytes
 
+  				H41R7_F45 Memory Map:	TODO must update linker file
+				- Application: Sectors 4 - 7: 		0x08020000 - 0x080FFFFF >> 896 KB
+		 	 	- Read-only (RO): Sectors 0 & 1: 	0x08000000 - 0x0800FFFF >> 64 KB, used to store topology information and Command Snippets
+				- Emulated EEPROM: Sectors 2 & 3: 	0x08010000 - 0x0801FFFF >> 64 KB, fits 1024 16-bit variables in 2 main-duplicate pages (A and B)
+*/
+#ifdef H41R7_F45
+	#define APP_START_ADDRESS  				((uint32_t)0x08020000)
+	#define RO_START_ADDRESS  				((uint32_t)0x08000000)
+	#define RO_MID_ADDRESS  				((uint32_t)0x08008000) 		// Snippets are stored here
+	#define EEPROM_START_ADDRESS  			((uint32_t)0x08010000)
+	#define FLASH_SIZE						((uint32_t)0xE0000)			// Size of area available to application: E0000. All sizes in bytes
+	#define SRAM_SIZE						((uint32_t)0x3C000)
+	#define PAGE_SIZE             			((uint32_t)0x8000)  		// 32K Sector size for emulated EEPROM erase operations - TODO review eeprom code
+	//#define NumOfPages						(FLASH_SIZE/PAGE_SIZE)
+#endif
 
 
 #ifdef __cplusplus
